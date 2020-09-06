b366a22ccdb1c0ed5029eea184bedbf69ad7593e
Olga Chyzh <ochyzh1@gmail.com>
2020-09-06 16:43:14 -0400

    help

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Collaborations among rebel groups
+#'
+#' A dataset to replicate analysis from Gade et al 2019
+#'
+#' @format A data frame with 930 rows and 11 variables:
+#' \describe{
+#'   \item{Var1}{group name of the first renel group}
+#'   \item{Var2}{group name of the second renel group}
+#'   \item{coopActions}{number of cooperative actions between the two groups}
+#'   \item{id}{dyad id}
+#'   \item{ideol_diff.dyad}{ideological distance between the two groups}
+#'   \item{powerdiff.dyad}{power difference between the two groups}
+#'   \item{loc.dyad}{location of the two groups}
+#'   \item{spons.dyad}{common sponsor}
+#'   \item{averageId.node}{first group's ideology}
+#'   \item{size.node}{first group's size}
+#'   \item{spons_actor.node}{first group's sponsor}
+#'   ...
+#' }
+#' @source Gade et al 2019
+"gadeData"
